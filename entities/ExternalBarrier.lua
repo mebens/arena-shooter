ebf794228c9d12d6335670a45587e6ffd27dd43e
Michael Ebens <michael@nova-fusion.com>
2013-08-08 13:27:23 +1000

    Fixed missiles clipping through external barriers.

--- entities/ExternalBarrier.lua
+++ entities/ExternalBarrier.lua
@@ -3,25 +3,21 @@ ExternalBarrier = class("ExternalBarrier", Barrier)
 function ExternalBarrier:initialize(type)
   Barrier.initialize(self, type)
   self.sides = {}
+  self.shapePadding = 50
 end
 
 function ExternalBarrier:added()
   Barrier.added(self)
   local w = self.world.width
   local h = self.world.height
-  local p = self.padding
+  local sp = self.shapePadding
+  local vp = self.padding
   
   if self.type == "rectangle" then
-    self.sides[1] = self:addShape(love.physics.newEdgeShape(p, p, w - p, p))
-    self.sides[2] = self:addShape(love.physics.newEdgeShape(w - p, p, w - p, h - p))
-    self.sides[3] = self:addShape(love.physics.newEdgeShape(p, h - p, w - p, h - p))
-    self.sides[4] = self:addShape(love.physics.newEdgeShape(p, p, p, h - p))
-    
-    -- old rect and shape padding code
-    -- self.top = self:addShape(love.physics.newRectangleShape(w / 2, (vp - p) / 2, w + p * 2, p))
-    -- self.bottom = self:addShape(love.physics.newRectangleShape(w / 2, h + (p - vp) / 2, w + p * 2, p))
-    -- self.left = self:addShape(love.physics.newRectangleShape((vp - p) / 2, h / 2, p, h + p * 2))
-    -- self.right = self:addShape(love.physics.newRectangleShape(w + (p - vp) / 2, h / 2, p, h + p * 2))
+    self.sides[1] = self:addShape(love.physics.newRectangleShape(w / 2, (vp - sp) / 2, w + sp * 2, sp))
+    self.sides[2] = self:addShape(love.physics.newRectangleShape(w / 2, h + (sp - vp) / 2, w + sp * 2, sp))
+    self.sides[3] = self:addShape(love.physics.newRectangleShape((vp - sp) / 2, h / 2, sp, h + sp * 2))
+    self.sides[4] = self:addShape(love.physics.newRectangleShape(w + (sp - vp) / 2, h / 2, sp, h + sp * 2))
   elseif self.type == "pentagon" then
     
   elseif self.type == "hexagon" then
